7416db5b56d1f46530d41a81ea171037347f92fc
ryannamgung <ryannamgung@gmail.com>
2018-09-07 15:35:32 -0400

    sprite edit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GameContainer from './containers/GameContainer'
+import {Sprite} from './components/Sprite'
+import sheet from './res/maple-foreground.png'
 
 class App extends Component {
   render() {
     return (
-      <GameContainer />
+      <div className='App'>
+        {Sprite({
+          filename: sheet,
+          x : 116,
+          y: 79,
+          width: 100, //range of
+          height : 260 //range of 85
+        })}
+      </div>
     );
   }
 }
